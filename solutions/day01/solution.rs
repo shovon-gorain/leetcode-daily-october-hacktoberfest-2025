b1afa056132bbdc5e29256c2366de5de7ce7bb81
DankJugal <iwhsvshsbsbs@gmail.com>
2025-10-01 23:56:07 +0530

    Added solution for Day 1 LC in rust lang

---
+++ solutions/day01/solution.rs
@@ -0,0 +1,14 @@
+struct Solution;
+
+impl Solution {
+    pub fn num_water_bottles(mut num_bottles: i32, num_exchange: i32) -> i32 {
+        let mut ans = num_bottles;
+        while num_bottles >= num_exchange {
+            ans += num_bottles / num_exchange;
+            let left = num_bottles % num_exchange;
+            num_bottles = num_bottles / num_exchange + left;
+        }
+        ans
+    }
+}
+
