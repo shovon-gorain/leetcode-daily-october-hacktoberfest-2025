2e5914cad4147e98c9e1c0b0cd63108b4dd39d7b
ziegluuu <155681447+ziegluuu@users.noreply.github.com>
2025-10-01 15:16:20 +0530

    add solution for day 1

---
+++ solutions/day01/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int ans = 0;
+
+        while (numBottles >= numExchange)
+        {
+            ans += numExchange;
+            numBottles -=  numExchange;
+            numBottles++;
+        }
+        
+
+
+        return ans + numBottles;
+    }
+};
