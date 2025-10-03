0ad082d10b990b0725803fa3fd4d165aeecde0e3
Nikunj Kohli <nikunjkohli10@gmail.com>
2025-10-03 11:54:11 +0530

    Problem 1

--- solutions/day01/solution.java
+++ solutions/day01/solution.java
@@ -1,14 +1,6 @@
-public class Solution {
+class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-        int total = numBottles;  // bottles we can drink
-        int empty = numBottles;  // bottles that become empty
-
-        while (empty >= numExchange) {
-            int newBottles = empty / numExchange;  // exchange empty for full
-            total += newBottles;                  // drink them
-            empty = empty % numExchange + newBottles; // update empty bottles
-        }
-
-        return total;
+        int sum = (numBottles-1)/(numExchange-1);
+        return sum+numBottles;
     }
 }
