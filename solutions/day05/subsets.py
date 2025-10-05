b90bbbea32a873bd01e0c38ade8aaf2fad010961
shivamkalra123 <kalrashivam47@gmail.com>
2025-10-05 12:23:25 +0530

    subsets.py

---
+++ solutions/day05/subsets.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result=[]
+        subset=[]
+        def sub(i):
+            if i>len(nums):
+                return
+            if i==len(nums):
+                result.append(subset.copy())
+                return
+            subset.append(nums[i])
+            sub(i+1)
+            subset.pop()
+            sub(i+1)
+        sub(0)
+        return result
+        
+            
+        
